a17b82df6fe4a4db47a45a346ee4395d916725cf
shouston3 <shouston3@me.com>
2017-06-23 21:11:50 +0100

    fixing script sed error messages by using  instead of

--- script.sh
+++ script.sh
@@ -5,10 +5,10 @@ if [ -f ~/.vimrc ]; then
 fi
 
 if [ -f ~/.zshrc ]; then
-  mv ~/.zshrc ~/.zshrc.old
+  cp ~/.zshrc ~/.zshrc.old
+else
+  touch ~/.zshrc
 fi
-
-export RAW_GH_URL="https://raw.githubusercontent.com/shouston3/my-config/master"
 
 sed -i "" 's/.*plugins=(.*/plugins=(git sams-aliases vi-mode)/g' ~/.zshrc
 sed -i "" 's/.*ZSH_THEME=.*/ZSH_THEME="sams-theme"/g' ~/.zshrc
@@ -16,11 +16,12 @@ sed -i "" 's/.*ZSH_THEME=.*/ZSH_THEME="sams-theme"/g' ~/.zshrc
 if [ ! -d ~/.oh-my-zsh/plugins/sams-aliases ]; then
   mkdir ~/.oh-my-zsh/plugins/sams-aliases
 fi
-curl -SLso "$RAW_GH_URL/sams-aliases.plugin.zsh" ~/.oh-my-zsh/plugins/sams-aliases/sams-aliases.plugin.zsh
 
-curl -SLso "$RAW_GH_URL/sams-theme.zsh-theme" ~/.oh-my-zsh/themes/sams-theme.zsh-theme
+export RAW_GH_URL="https://raw.githubusercontent.com/shouston3/my-config/master"
 
-curl -SLso "$RAW_GH_URL/.vimrc" ~/.vimrc
+curl -SLs "$RAW_GH_URL/sams-aliases.plugin.zsh" > ~/.oh-my-zsh/plugins/sams-aliases/sams-aliases.plugin.zsh
+curl -SLs "$RAW_GH_URL/sams-theme.zsh-theme" > ~/.oh-my-zsh/themes/sams-theme.zsh-theme
+curl -SLs "$RAW_GH_URL/.vimrc" > ~/.vimrc
 
 # Setup vim-pathogen
 mkdir -p ~/.vim/autoload ~/.vim/bundle
